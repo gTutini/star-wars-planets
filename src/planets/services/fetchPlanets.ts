3a5690bdf6b6658b39f5e8247a8f5a3121d4c3f1
Giancarlo Tutini <tutini.giancarlo@gmail.com>
2025-10-30 02:50:41 -0300

    feat(fetchPlanets): add pagination support to fetchPlanets function

--- src/planets/services/fetchPlanets.ts
+++ src/planets/services/fetchPlanets.ts
@@ -22,11 +22,18 @@ export interface PlanetsResponse {
   results: Planet[];
 }
 
-export async function fetchPlanets(search?: string): Promise<PlanetsResponse> {
+export async function fetchPlanets(
+  search?: string,
+  page?: string
+): Promise<PlanetsResponse> {
   const url = new URL(`${process.env.NEXT_PUBLIC_API_URL}/planets`);
 
   if (search) {
     url.searchParams.set("search", search);
+  }
+
+  if (page) {
+    url.searchParams.set("page", page);
   }
 
   const res = await fetch(url.toString());
