1aba1c09c156774efb652a15e532e30e5c1f0a6b
Giancarlo Tutini <tutini.giancarlo@gmail.com>
2025-10-30 01:32:17 -0300

    feat(Home): implement Home component with planet and film data fetching and display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
-export default function Home() {
-  return <h1>Olá mundo :)</h1>;
+import { Flex, Grid, Heading, Section } from "@radix-ui/themes";
+
+import { SvgOrderSymbol } from "@/icons";
+import { fetchFilms, fetchPlanets } from "@/planets/services";
+import { PlanetCard } from "@/planets/components";
+
+import styles from "./page.module.scss";
+
+export default async function Home() {
+  const planets = await fetchPlanets();
+  const films = await fetchFilms();
+
+  return (
+    <Section size="1">
+      <Flex direction="column" align="center" className={styles.content}>
+        <SvgOrderSymbol fill="#ff949d" />
+        <Heading align="center" size="8">
+          The Jedi Archives: Planets
+        </Heading>
+      </Flex>
+      <Grid
+        mt="9"
+        columns="repeat(auto-fit, minmax(250px, 1fr))"
+        gapX="4"
+        gapY="5"
+      >
+        {planets.results.map((planet) => (
+          <PlanetCard key={planet.name} planet={planet} films={films.results} />
+        ))}
+      </Grid>
+    </Section>
+  );
 }
