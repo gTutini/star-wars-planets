c0eb95ec423e3f60cd86c4d5b76fdde660578abb
Giancarlo Tutini <tutini.giancarlo@gmail.com>
2025-10-30 01:28:41 -0300

    feat(layout): wrap body in Theme and Container components for improved styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "../styles/globals.scss";
+import { Container, Theme } from "@radix-ui/themes";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -19,7 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
-      <body className={`${montserrat.variable}`}>{children}</body>
+      <body className={`${montserrat.variable}`}>
+        <Theme
+          radius="full"
+          accentColor="ruby"
+          appearance="dark"
+          panelBackground="translucent"
+        >
+          <Container mb="9" px="4" asChild>
+            <main>{children}</main>
+          </Container>
+        </Theme>
+      </body>
     </html>
   );
 }
