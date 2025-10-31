77d22b08519583c753884fa4e731a3c6d9844dc6
Giancarlo Tutini <tutini.giancarlo@gmail.com>
2025-10-30 21:46:07 -0300

    feat(Films): add Film interface and fetchFilms service for film data retrieval

--- src/films/services/fetchFilms.ts
+++ src/films/services/fetchFilms.ts
@@ -1,20 +1,4 @@
-// Tipos para a resposta da API SWAPI - Films
-export interface Film {
-  title: string;
-  episode_id: number;
-  opening_crawl: string;
-  director: string;
-  producer: string;
-  release_date: string;
-  characters: string[];
-  planets: string[];
-  starships: string[];
-  vehicles: string[];
-  species: string[];
-  created: string;
-  edited: string;
-  url: string;
-}
+import { Film } from "../contracts";
 
 export interface FilmsResponse {
   count: number;
