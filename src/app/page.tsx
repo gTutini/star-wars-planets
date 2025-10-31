f6f65c8a1f9e9463536994b7d63b33e1ce478bf4
Giancarlo Tutini <tutini.giancarlo@gmail.com>
2025-10-30 23:11:48 -0300

    refactor(Home): remove unused styles and components for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from "react";
-import { Flex, Heading, Section } from "@radix-ui/themes";
+import { Flex, Section } from "@radix-ui/themes";
 
-import { SvgOrderSymbol } from "@/icons";
 import {
   PlanetList,
   PlanetListSkeleton,
   SearchInput,
 } from "@/planets/components";
-
-import styles from "./page.module.scss";
 
 interface HomeProps {
   searchParams: Promise<{ search?: string; page?: string }>;
@@ -19,12 +16,6 @@ export default async function Home({ searchParams }: HomeProps) {
 
   return (
     <Section size="1">
-      <Flex direction="column" align="center" className={styles.content}>
-        <SvgOrderSymbol fill="#ff949d" />
-        <Heading align="center" size="8">
-          The Jedi Archives: Planets
-        </Heading>
-      </Flex>
       <Flex direction="column" mt="6">
         <SearchInput defaultValue={search} />
       </Flex>
