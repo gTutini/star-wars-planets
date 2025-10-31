6804dbd50a7f9ca9050efd5c37f83c491a50791a
Giancarlo Tutini <tutini.giancarlo@gmail.com>
2025-10-30 21:48:19 -0300

    feat(contracts): add interfaces for Planet, Resident, Species, and Vehicle

---
+++ src/planets/contracts.ts
@@ -0,0 +1,71 @@
+export interface Planet {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  residents: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface Resident {
+  birth_year: string;
+  eye_color: string;
+  films: string[];
+  gender: string;
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  mass: string;
+  name: string;
+  skin_color: string;
+  created: string;
+  edited: string;
+  species: string[];
+  vehicles: string[];
+  url: string;
+}
+
+export interface Species {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: string;
+  average_lifespan: string;
+  eye_colors: string;
+  hair_colors: string;
+  skin_colors: string;
+  language: string;
+  homeworld: Planet;
+  people: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface Vehicle {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+  passengers: string;
+  cargo_capacity: string;
+  consumables: string;
+  vehicle_class: string;
+  pilots: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
