8d71d6cccb37cf5d791966162923805d6ba4d76c
Giancarlo Tutini <tutini.giancarlo@gmail.com>
2025-10-30 22:13:53 -0300

    refactor(contracts): move Resident interface to a separate file for better organization

--- src/planets/contracts.ts
+++ src/planets/contracts.ts
@@ -15,24 +15,6 @@ export interface Planet {
   url: string;
 }
 
-export interface Resident {
-  birth_year: string;
-  eye_color: string;
-  films: string[];
-  gender: string;
-  hair_color: string;
-  height: string;
-  homeworld: string;
-  mass: string;
-  name: string;
-  skin_color: string;
-  created: string;
-  edited: string;
-  species: string[];
-  vehicles: string[];
-  url: string;
-}
-
 export interface Species {
   name: string;
   classification: string;
